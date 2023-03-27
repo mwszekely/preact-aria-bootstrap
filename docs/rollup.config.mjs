4c91ae65eeb0a823a2529f09ead8c348e88a6cd2
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-03-27 11:38:32 -0400

    Fix source maps in demo

--- docs/rollup.config.mjs
+++ docs/rollup.config.mjs
@@ -1,13 +1,14 @@
-import resolve from '@rollup/plugin-node-resolve';
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import { babel } from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
 import path from "path";
-import sourcemaps from "rollup-plugin-sourcemaps"
+import sourcemaps from "rollup-plugin-sourcemaps";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
+/** @type {import('rollup').RollupOptions} */
 export default {
     input: "index.tsx",
     output: {
@@ -16,18 +17,16 @@ export default {
         name: "bundle",
         sourcemap: true,
     },
+    treeshake: "recommended",
     plugins: [
-        typescript({ sourceMap: true }), 
-        replace({ 'process.env.NODE_ENV': JSON.stringify('development'), preventAssignment: false }),
-        commonjs({ sourceMap: true, transformMixedEsModules: true, extensions }),
-        resolve({ extensions, dedupe: ['preact', "preact/compat", "preact/hooks"] }),   // TODO: Why, exactly, is dedupe needed? It doesn't not make sense, but could the Preact error be avoided?
-        babel({
-            configFile: path.resolve(".babelrc"),
-            sourceMaps: true,
-            babelHelpers: "bundled",
-            extensions
+        typescript(),
+        replace({ 'process.env.NODE_ENV': JSON.stringify('development'), preventAssignment: true }),
+        commonjs({ extensions, sourceMap: true, transformMixedEsModules: true }),
+        resolve({ extensions, dedupe: ['preact', "preact/compat", "preact/hooks"] }),   // TODO: Why, exactly, is dedupe needed? It doesn't not make sense, but specifically. Why.
+        getBabelOutputPlugin({  // Used instead of babel because babel generates incorrect source maps for our code (but not Preact's)
+            allowAllFormats: true,                  // Needed because we output in iife for this demo
+            configFile: path.resolve(".babelrc"),   // getBabelOutputPlugin doesn't use the default config file                  
         }),
-        sourcemaps()    // TODO: This is deprecated but needed for TS source maps
+        sourcemaps(),    // TODO: This is deprecated but needed for both Preact's and our own TS source maps.
     ],
 }
-
